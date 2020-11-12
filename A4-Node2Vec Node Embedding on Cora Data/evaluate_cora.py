997bcad8abb5490e99ce482bbdefec9a3baefdc7
Menghong Han <61367724+menghonghan@users.noreply.github.com>
2020-11-11 23:13:16 -0500

    Add files via upload

---
+++ A4-Node2Vec Node Embedding on Cora Data/evaluate_cora.py
@@ -0,0 +1,119 @@
+#-----------------------------------------------------------------------------------------------#
+#                                                                                               #
+#   I M P O R T     L I B R A R I E S                                                           #
+#                                                                                               #
+#-----------------------------------------------------------------------------------------------# 
+
+import numpy as np
+import sys
+import scipy.sparse as sp
+import networkx as nx
+import argparse
+from collections import defaultdict
+from gensim.models import Word2Vec, KeyedVectors
+from six import iteritems
+from sklearn.linear_model import LogisticRegression
+from scipy.io import loadmat
+
+
+#-----------------------------------------------------------------------------------------------#
+#                                                                                               #
+#   Create dictionary (graph) our of sparse matrix                                              #
+#                                                                                               #
+#-----------------------------------------------------------------------------------------------# 
+def sparse2graph(x):
+    G = defaultdict(lambda: set())
+    cx = x.tocoo()
+    for i,j,v in zip(cx.row, cx.col, cx.data):
+        G[i].add(j)
+    return {str(k): [str(x) for x in v] for k,v in iteritems(G)}
+
+
+#-----------------------------------------------------------------------------------------------#
+#                                                                                               #
+#   HELPER FUNCTIONS FOR SPLITTING DATA TRAIN, VALIDATION, TEST                                 #
+#                                                                                               #
+#-----------------------------------------------------------------------------------------------# 
+def get_splits():
+    idx_train = range(200)
+    idx_val = range(200, 500)
+    idx_test = range(500, 1500)
+    return idx_train, idx_val, idx_test
+
+def format_csr(y_):
+  y = [[] for x in range(y_.shape[0])]
+
+  cy =  y_.tocoo()
+  for i, j in zip(cy.row, cy.col):
+      y[i].append(j)
+  return y
+
+
+
+#-----------------------------------------------------------------------------------------------#
+#                                                                                               #
+#   EVALUATE                                                                                    #
+#   Perform Logistic Regression of embedding                                                    #
+#                                                                                               #
+#-----------------------------------------------------------------------------------------------# 
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--emb", required=True)
+  parser.add_argument("--net", required=True)
+  parser.add_argument("--labels", required=True)
+  parser.add_argument('--dic-network-name', default='network')
+  parser.add_argument('--dic-label-name', default='label')
+
+  args = parser.parse_args()
+
+
+  ## Load Embeddings
+  embeddings_file = args.emb
+  model = KeyedVectors.load_word2vec_format(embeddings_file, binary=False)
+  
+  ## Load labels
+  g_npz = sp.load_npz(args.net)
+  graph = nx.from_scipy_sparse_matrix(g_npz)
+  labels_matrix = sp.load_npz(args.labels)
+  
+  labels_count = labels_matrix.shape[1]
+  
+  # Map nodes to their features (note:  assumes nodes are labeled as integers 1:N)
+  features_matrix = np.asarray([model[str(node)] for node in range(len(graph))])
+
+  ## Split in training, validation, test set
+  X, y = features_matrix, labels_matrix
+
+  idx_train, idx_val, idx_test = get_splits()
+  y_train_ = y[idx_train]
+  y_val_ = y[idx_val]
+  y_test_ = y[idx_test]
+  X_train = X[idx_train]
+  X_test = X[idx_test]
+
+  y_train = format_csr(y_train_)
+  y_test = format_csr(y_test_)
+
+  ## Logistic Regression
+
+  # Train on data
+  logisticRegr = LogisticRegression()
+  logisticRegr.fit(X_train, y_train)
+
+  # Measure accuracy
+  score = logisticRegr.score(X_test, y_test)
+
+  # Output results
+  print ('---------------------------------')
+  print ('Accuracy Score :   ', score)
+  print ('---------------------------------')
+
+
+
+#-----------------------------------------------------------------------------------------------#
+#                                                                                               #
+#   Start                                                                                       #
+#                                                                                               #
+#-----------------------------------------------------------------------------------------------# 
+if __name__ == "__main__":
+  sys.exit(main())
---
+++ A4-Node2Vec Node Embedding on Cora Data/node2vec/main.py
@@ -0,0 +1,67 @@
+#-----------------------------------------------------------------------------------------------#
+#                                                                                               #
+#   I M P O R T     L I B R A R I E S                                                           #
+#                                                                                               #
+#-----------------------------------------------------------------------------------------------# 
+import argparse
+import time
+from gensim.models import Word2Vec
+from multiprocessing import cpu_count
+
+import graph
+
+#-----------------------------------------------------------------------------------------------#
+#                                                                                               #
+#   NODE2VEC                                                                                    #
+#                                                                                               #
+#-----------------------------------------------------------------------------------------------#
+def node2vec(args):
+	start_time = time.time()
+
+	# Init graph
+	G = graph.Graph(args.input, args.p, args.q)
+
+	total_walks = G.G.number_of_nodes() * args.num_walks
+	data_size = total_walks * args.walk_length
+
+	print("\nNumber of nodes: {}".format(G.G.number_of_nodes()))
+	print("Total number of walks: {}".format(total_walks)) # Number of walks starting from each node
+	print("Data size (walks*length): {}\n".format(data_size))
+
+	# Create the random walks and store them in walks list
+	print("Generate walks ...")
+
+	G.preprocess_transition_probs()
+	walks = G.build_node2vec_walks(args.num_walks, args.walk_length)
+
+	# Feed to walks to Word2Vec model
+	walks = [list(map(str, walk)) for walk in walks]
+	model = Word2Vec(walks, size=args.dimension, window=5, min_count=0, sg=1, workers=cpu_count(), iter=args.iter)
+
+  	# Save to output file
+	print("----- Total time {:.2f}s -----".format(time.time() - start_time))
+	model.wv.save_word2vec_format(args.output)
+	return
+
+#-----------------------------------------------------------------------------------------------#
+#                                                                                               #
+#   MAIN                                                                                        #
+#   Parse input args                                                                            #
+#                                                                                               #
+#----------------------------------------------------------------------------------------------# 
+def main():
+	parser = argparse.ArgumentParser(description="Run node2vec.")
+	parser.add_argument('--input', nargs='?', required=True)
+	parser.add_argument('--output', nargs='?', required=True)
+	parser.add_argument('--walk-length', type=int, default=40)
+	parser.add_argument('--num-walks', type=int, default=10)
+	parser.add_argument('--dimension', type=int, default=128, help='Embeddings dimension')
+	parser.add_argument('--iter', default=1, type=int, help='Number of epochs in SGD')
+	parser.add_argument('--p', type=float, default=0.5, help='Return hyperparameter')
+	parser.add_argument('--q', type=float, default=2, help='Input hyperparameter')
+	args = parser.parse_args()
+
+	node2vec(args)
+
+if __name__ == "__main__":
+	main()
